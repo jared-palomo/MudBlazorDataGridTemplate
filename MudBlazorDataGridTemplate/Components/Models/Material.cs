0d07a64ebde842bf1c8b0cb4d3b799f58c431a2d
Jared Palomo <jared.palomo@ds-prima.com.au>
2024-12-02 12:20:57 -0600

    Add project files.

---
+++ MudBlazorDataGridTemplate/Components/Models/Material.cs
@@ -0,0 +1,17 @@
+﻿namespace MudBlazorDataGridTemplate.Components.Models;
+
+public class Material
+{
+    public string? DisplayName { get; set; }
+    public string? Description { get; set; }
+    public string? Batching { get; set; }
+    public string? DefaultUnitOfWeight { get; set; }
+    public string? ItemIdNumberFormat { get; set; }
+    public string? NextBatchId { get; set; }
+    public Guid? SampleOfMaterialId { get; set; }
+    public int? ItemIdExpectedInheritances { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public DateTime ModifiedOn { get; set; }
+    public string? ModifiedBy { get; set; }
+    public bool IsActive { get; set; }
+}
